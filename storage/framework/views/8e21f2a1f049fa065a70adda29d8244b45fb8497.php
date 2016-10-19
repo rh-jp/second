31e860685fc78f80fe91b4382a8450f7c9420d7a
Jan Plas <janplas@iMac.local>
2016-10-19 17:24:32 +0200

    zoekfunctie update

--- storage/framework/views/8e21f2a1f049fa065a70adda29d8244b45fb8497.php
+++ storage/framework/views/8e21f2a1f049fa065a70adda29d8244b45fb8497.php
@@ -19,17 +19,9 @@
 
                         <div class="input-group">
                         <div class="input-group-btn search-panel">
-                            <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+                            <button type="button" class="btn btn-default" disabled="">
                                 <span id="search_concept">www.</span> <span class="caret" style="margin-left: 5px;"></span>
                             </button>
-                            <ul class="dropdown-menu" role="menu">
-                              <li><a href="#contains">Contains</a></li>
-                              <li><a href="#its_equal">It's equal</a></li>
-                              <li><a href="#greather_than">Greather than ></a></li>
-                              <li><a href="#less_than">Less than < </a></li>
-                              <li class="divider"></li>
-                              <li><a href="#all">Anything</a></li>
-                            </ul>
                         </div>
                         <input type="hidden" name="search_param" value="all" id="search_param">         
                         <input type="text" class="form-control" name="q" placeholder="Zoek domeinnaam...">
